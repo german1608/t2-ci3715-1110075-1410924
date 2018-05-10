d9d1a9950f637884ef86d7ad32a986675bf0a7da
jonbandes <jonathanbandes@gmail.com>
2018-05-10 11:09:39 -0400

    Update Tarifa.py

--- Tarifa.py
+++ Tarifa.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/python
-from decimal import Decimal
-import datetime
-
 
 class Tarifa():
-
-	def __init__(self, tarifa_entre_semana, tarifa_fin_semana):
-		self.tarifa_entre_semana = tarifa_entre_semana
-		self.tarifa_entre_semana = tarifa_fin_semana
+   pass
